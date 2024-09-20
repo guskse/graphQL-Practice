39b609dbbf2b6faae2d40d8730645b36663d68e1
gustavo <guskse@gmail.com>
2024-09-20 10:23:57 -0400

    added Mutations and Resolvers

--- schema.js
+++ schema.js
@@ -3,24 +3,51 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]! # array of String and it can't be null
+        reviews: [Review!]
     }
     type Review {
         id: ID!
-        rating: Int!
+        rating: Float!
         content: String!
+        game: Game!
+        author: Author!
     }
     type Author {
         id:ID!
         name:String!
         verified:Boolean
+        reviews: [Review!]
     }
 
-    #Entry Points (this is required)
+    #Entry Points (this is required for fetching data GET requests)
     type Query {
         reviews: [Review]
+        review(id: ID!): Review
+
         games: [Game]
+        game(id: ID!): Game
+        
         authors: [Author]
+        author(id: ID!): Author
     }
+
+    #mutations will allow us to POST, PUT and DELETE data
+    type Mutation {
+        addGame(game: AddGameInput!): Game
+        deleteGame(id:ID!): [Game]
+        updateGame(id:ID!, edits: EditGameInput!): Game
+    }
+
+    input AddGameInput {
+        title: String!,
+        platform: [String!]!
+    }
+
+    input EditGameInput {
+        title:String
+        platform:[String!]
+    }
+    
 `;
 
 //graphQL data types we can use:
